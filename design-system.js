198d64c750f1216168e6b3186b32960c6211c59f
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2024-02-05 23:45:51 -0800

    Close button position and script for hamburger menu

---
+++ design-system.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerIcon = document.querySelector('.navbar__hamburger-icon');
+    const closeButton = document.querySelector('.close-button');
+    const navLinks = document.querySelector('.navbar__nav-links');
+    const sidebar = document.querySelector('.sidebar');
+
+    hamburgerIcon.addEventListener('click', function() {
+        navLinks.style.display = 'block';
+        sidebar.style.display = 'block';
+        closeButton.style.display = 'block';
+        hamburgerIcon.style.display = 'none';
+    });
+
+    closeButton.addEventListener('click', function() {
+        navLinks.style.display = 'none';
+        sidebar.style.display = 'none';
+        closeButton.style.display = 'none';
+        hamburgerIcon.style.display = 'block';
+    });
+});
+  
